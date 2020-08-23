7dfc203dbfd8500134d3364ed37cd7923d33480b
yuyang <yuyang9@100tal.com>
2020-08-23 16:16:48 +0800

    test

---
+++ src/AesEncrypt.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoyou
+ * Date: 2020/8/23
+ * Time: 3:12 PM
+ */
+
+namespace AesEncrypt;
+
+
+class AesEncrypt
+{
+
+    public function myTest()
+    {
+        echo 'test';
+        return null;
+    }
+}
---
+++ test/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoyou
+ * Date: 2020/8/23
+ * Time: 4:13 PM
+ */
+
+require  "./vendor/autoload.php";
+
+$test = new \AesEncrypt\AesEncrypt();
+$test->myTest();
